2c6fe24ce467fd2c0060d29bdb7e6dc85f2d3e90
NishiReina <s1f101901195@iniad.org>
2022-11-10 18:51:04 +0900

    fix

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,6 +3,9 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
+echo "vite"
+npm run build
+
 echo "Caching config..."
 php artisan config:cache
 
